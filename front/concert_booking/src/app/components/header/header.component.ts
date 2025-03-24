5f262c37a24f03f8a2c44efb59edc2803a19ed0f
Irina NAHOANIKO <irina.nahoaniko@gmail.com>
2025-03-24 14:56:15 +0100

    adding search bar and logo header

--- front/concert_booking/src/app/components/header/header.component.ts
+++ front/concert_booking/src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchBarComponent,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  onSearch(query: string) {
+    console.log('Recherche :', query);
+    // → Appel à ton service / filtre local / route, etc.
+  }
 
 }
